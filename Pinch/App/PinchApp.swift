0fa8b0886e62b2acffe5803e226a7f0914189f29
Shubham Kumar <kumar.shubham1329@gmail.com>
2022-05-07 20:07:30 +0530

    Initial Commit

---
+++ Pinch/App/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Shubham Kumar on 07/05/22.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
