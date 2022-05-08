db6f99f0f3ad9b8de88291f588f3c5391fce79f9
Shubham Kumar <kumar.shubham1329@gmail.com>
2022-05-08 11:02:31 +0530

    Scale Effect added on Image

--- Pinch/App/ContentView.swift
+++ Pinch/App/ContentView.swift
@@ -8,9 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isAnimating: Bool = false
+    @State private var imageScale: CGFloat = 1
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ZStack {
+                //MARK: Page Image
+                Image("magazine-front-cover")
+                    .resizable()
+                    .scaledToFit()
+                    .cornerRadius(10)
+                    .padding()
+                    .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
+                    .opacity(isAnimating ? 1 : 0)
+                    .scaleEffect(imageScale)
+                //MARK: Tap Gesture
+                    .onTapGesture(count: 2) {
+                        if imageScale == 1 {
+                            withAnimation(.spring()) {
+                                imageScale = 5
+                            }
+                        } else {
+                            withAnimation(.spring()) {
+                                imageScale = 1
+                            }
+                        }
+                    }
+            }
+            .navigationTitle("Pinch & Zoom")
+            .navigationBarTitleDisplayMode(.inline)
+            .onAppear {
+                withAnimation(.linear(duration: 1)) {
+                    isAnimating = true
+                }
+            }
+        }//:NavigationView
+        .navigationViewStyle(.stack)
     }
 }
 
